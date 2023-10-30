14ddca2d3270bd7a4e68b55ba71fc660156f4bde
Mikayel Arzumanyan <mikarzum@c2r2s5.42yerevan.am>
2023-10-30 15:56:03 +0400

    dsa

---
+++ ScalarConverter.cpp
@@ -0,0 +1,15 @@
+
+
+static class ScalarConverter
+{
+	public:
+		ScalarConverter();
+		ScalarConverter(const ScalarConverter& obj);
+		ScalarConverter& operator=(const ScalarConverter& obj);
+		~ScalarConverter();
+	public:
+		void convert();
+		void convert();
+		void convert();
+		void convert();
+};
